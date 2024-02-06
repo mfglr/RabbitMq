adbc3e1a37caa411fc34db401bc0fa4fe55c47a6
mfglr <mfgglr@outlook.com>
2024-02-06 22:06:40 +0300

    frist queue

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://splvvplc:cIISajOBUOptA35p1cIPCfcZ6TIrwIGW@rattlesnake.rmq.cloudamqp.com/splvvplc");
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+string queueName = "first_queue";
+
+channel.QueueDeclare(
+	queueName,
+	true,
+	false,
+	false
+);
+
+string messaj = "first message";
+var messageBody = Encoding.UTF8.GetBytes(messaj);
+
+//default exchange
+channel.BasicPublish(string.Empty, queueName, null, messageBody);
+Console.WriteLine("The message sent");
+Console.ReadLine();
+
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://splvvplc:cIISajOBUOptA35p1cIPCfcZ6TIrwIGW@rattlesnake.rmq.cloudamqp.com/splvvplc");
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+string queueName = "first_queue";
+
+//If you are sure that the publisher created the queue, the following line can be deleted.
+channel.QueueDeclare( queueName, true, false, false );
+
+var subscriber = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queueName, false, subscriber);
+
+subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+	var message = Encoding.UTF8.GetString(e.Body.ToArray());
+	Console.WriteLine(message);
+};
+
+
+Console.ReadLine();
+
+
